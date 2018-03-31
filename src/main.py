c79312a4fba9eb0fd9840fee568e89813ab72a9b
thmslmr <thomas.lemeur@edu.esiee.fr>
2018-04-01 01:29:32 +0200

    Make class more usable

--- src/main.py
+++ src/main.py
@@ -6,118 +6,113 @@ from googleplaces import GooglePlaces
 
 class PlacesScrapper:
 
-    def __init__(self, places):
+    def __init__(self,
+                 services=['facebook', 'wikipedia', 'google'],
+                 settings_file='settings.json'):
         """ Init scrapper """
-        if not places:
-            raise Exception('Array of places must be provided.')
+        if not isinstance(services, list):
+            raise Exception('Services must be a list.')
 
-        self.input_places = places
-        self.output_places = []
-        self.settings = self.__fetch_settings()
-        self.google_places = GooglePlaces(self.settings["google_api_key"])
+        self.places = []
+        self.settings = self.__fetch_settings(settings_file)
+        self.services = [service for service in services
+                         if service in self.settings['services'].keys()]
+        if not services:
+            raise Exception('List of available services must be provided.')
 
-    def __fetch_settings(self):
+    def __fetch_settings(self, file_name):
         """ Fetch settings from settings.json"""
+        if not file_name:
+            raise Exception('Settings file name must be provided.')
+
         try:
-            with open('settings.json', 'r') as file:
-                settings = json.load(file)
+            with open(file_name, 'r') as file:
+                settings_file = json.load(file)
         except FileNotFoundError:
-            raise Exception('Sources file must be provided.')
+            raise Exception('Settings file must be provided.')
+
+        try:
+            settings = settings_file['placesscrapper']
+        except KeyError:
+            raise Exception('Settings file must contain \
+            `placesscrapper` settings.')
 
         return settings
 
-    def scrap(self):
-        print("==== START ====")
+    def __get_settings(self, path):
+        """ Get settings with dot path """
+        if not path:
+            raise Exception('Setting key must be provided')
 
-        for place_name in self.input_places:
-            place = self.get_google_info(place_name)
-            place['wikipedia'] = self.get_wikipedia_infos(place_name)
-            place['facebook'] = self.get_facebook_page_infos(place_name)
-            self.output_places.append(place)
-            self.dump_datas()
-            print(place_name + ' : done !')
+        res = self.settings
 
-        print("==== END ====")
+        for key in path.split('.'):
+            res = res.get(key, {})
 
-    def get_google_info(self, name):
-        """
-            Collect Google informations of the place with a text search request
-            Return a associative array with its id - website - google map url
-            - phone number - address
-        """
-        results = self.google_places.text_search(name)
-        google_infos = {}
+        return res
 
-        if len(results.places) == 0:
-            print(name + ' : Not found by Google Places!')
-        else:
-            matchObj = {
-                'google_id': 'id',
-                'website': 'website',
-                'google_map_url': 'url',
-                'phone_number': 'international_phone_number',
-                'formatted_address': 'formatted_address'
-            }
+    def get_google_infos(self, name):
+        """ Get Google infos of the first place for a given name """
+        if not name:
+            raise Exception('Place name must be provided.')
 
-            place = results.places[0]
-            place.get_details()
-            for el in matchObj:
-                try:
-                    google_infos[el] = place.details[matchObj[el]]
-                except KeyError:
-                    google_infos[el] = False
-            google_infos['name'] = name
+        api_key = self.__get_settings("services.google.api_key")
+        gp = GooglePlaces(api_key)
+        results = gp.text_search(name)
 
-        return google_infos
+        if not results.places:
+            raise Exception('Place not found by Google.')
+
+        place = results.places[0]
+        place.get_details()
+
+        return {info: place.details.get(info) for info
+                in self.__get_settings('services.google.infos')}
 
     def get_wikipedia_infos(self, name):
-        """
-            Collect Wikipedia informations of the place or of
-            the first result of a Wikipedia research
-            Return a associative array with its title
-            page - wikipedia url - summmary
-        """
-        wiki_infos = {}
+        """ Get Wikipedia infos of the first place for a given name """
+        if not name:
+            raise Exception('Place name must be provided.')
+
         results = wikipedia.search(name)
 
-        if len(results) == 0:
-            print(name + ' : Not found by Wikipedia !')
-        else:
-            try:
-                page = wikipedia.page(name)
-            except wikipedia.exceptions.PageError:
-                page = wikipedia.page(results[0])
+        if not results:
+            raise Exception('Place not found by Wikipedia.')
 
-            wiki_infos['title'] = page.title
-            wiki_infos['url'] = page.url
-            wiki_infos['summary'] = page.summary
+        page = wikipedia.page(results[0])
 
-        return wiki_infos
+        return {info: page[info] for info
+                in self.__get_settings('services.wikipedia.infos')}
 
-    def get_facebook_page_infos(self, name):
-        facebook_info = {}
-        domain = 'https://graph.facebook.com/v2.8/'
-        fb_access_token = self.settings['facebook_api_key']
+    def get_facebook_infos(self, name):
+        """ Get Facebook infos for a given place name """
+        if not name:
+            raise Exception('Place name must be provided.')
 
-        url_graph_search = domain + 'search?q=' + name + \
+        domain = self.__get_settings('services.facebook.domain')
+        fb_access_token = self.__get_settings('services.facebook.api_key')
+
+        search_url = domain + 'search?q=' + name + \
             '&fields=id,fan_count&type=page&access_token=' + fb_access_token
-        res_search = requests.get(url_graph_search).json()
 
-        if len(res_search['data']) == 0:
-            print(name + ' : Not found by Facebook')
-        else:
-            target_page = self.get_max_fan_count_page(res_search['data'])
+        search_results = requests.get(search_url).json()
 
-            url_graph_page = domain + target_page['id'] + \
-                '?fields=about,cover,fan_count,general_info,link,name,\
-                picture.type(large)&access_token=' + fb_access_token
-            facebook_info = requests.get(url_graph_page).json()
+        if not search_results:
+            raise Exception('Place not found by Facebook.')
 
-        return facebook_info
+        page = max(search_results['data'], key=lambda x: x['fan_count'])
+        fields = ','.join(self.__get_settings('services.facebook.infos'))
+        url = '{0}{1}?fields={2}&access_token={3}'.format(
+            domain, page['id'], fields, fb_access_token)
 
-    def get_max_fan_count_page(self, pages):
-        max_fancount_page = pages[0]
-        for el in pages:
-            if el['fan_count'] > max_fancount_page['fan_count']:
-                max_fancount_page = el
-        return max_fancount_page
+        return requests.get(url).json()
+
+    def scrap(self, places):
+        """ Main function """
+        if not places:
+            raise Exception('Array of places must be provided.')
+
+        return [{"name": place, "infos": {
+            service: getattr(self, 'get_{0}_infos'.format(service))(place)
+            for service in self.services}}
+            for place in places]
